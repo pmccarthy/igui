64d79cb84beb8370ac3a1d1ffbaeed78daace55d
pmccarthy <paul.mccarthy@feedhenry.com>
2015-04-28 17:04:20 +0100

    Working with form submission and returning JSON template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var parser = require("../lib/parser")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/submit', function(req, res) {
+  console.log(req.body)
+  parser.generateOrgTemplate(req.body, function(err, data){
+	  if (err) {
+	  	return res.send(err);
+	  }
+	  	return res.send(data);
+  })
 });
 
 module.exports = router;
